7c021fc04fc670e34a0b676da4f1817839f8b31e
AnikinNikita <nikita.anikin@neuro-city.ru>
2020-11-02 18:10:12 +0300

    for in record

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn main() {    
+    env_logger::init();
+
+    let dataset = "./census.csv";
+    println!("Reading dataset from{}", dataset);
+    let file = fs::File::open(dataset).expect("Cannot read dataset");
+    let mut csv_reader = csv::Reader::from_reader(file);
+
+    for record in csv_reader.records() {
+        let record = record.expect("Invalid record");
+        let city = record.get(0);
+        let population = record.get(1);
+
+        log::trace!("{:?} population: {:?}", city, population)
+    }
 }
