64f19a33cb19ae0722d9397947161ec2870e665b
GreedoRen <greedo.it@gmail.com>
2020-12-03 22:38:30 +0300

    boolean

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
 fn main() {
+    //integer signed
     let x = 5;
 
     let x = x + 1;
 
     let x = x * 2;
 
+
+    //integer unsigned
     let guess: u32 = "42".parse().expect("Not a number!");
 
     let guess = guess + 22;
     let guess = "6".parse::<u32>();
 
+    //floating
     let x = 2.0;
     let y:f32 = 3.0;
 
     println!("The value of x is: {}", x);
 
     println!("{:?}", guess);
+
+    //boolean
+    let t = true;
+
 }
