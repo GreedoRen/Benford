05d74dd92f74fe44aa526ab3875c4e45b60cf8f3
GreedoRen <greedo.it@gmail.com>
2020-12-04 23:26:30 +0300

    return value with parameter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
    let x = five();
    let y = plus_one(1);
+   let z = two();
+   let n = minus_three(1);
 
    println!("{}", x);
    println!("{}", y);
+   println!("{}", z);
+   println!("{}", n);
 }
 
 fn five() -> i32 {
@@ -12,4 +16,12 @@ fn five() -> i32 {
 
 fn plus_one(y:i32) -> i32 {
     y + 1
+}
+
+fn two() -> u32 {
+    2
+}
+
+fn minus_three(n:i32) -> i32 {
+    n - 3
 }
