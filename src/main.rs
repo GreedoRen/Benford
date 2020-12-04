29dcf27eb27c7dbb03d11ad7cfe367a114c862b3
GreedoRen <greedo.it@gmail.com>
2020-12-04 23:26:54 +0300

    comment

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,3 @@
 fn main() {
-   let x = five();
-   let y = plus_one(1);
-   let z = two();
-   let n = minus_three(1);
-
-   println!("{}", x);
-   println!("{}", y);
-   println!("{}", z);
-   println!("{}", n);
-}
-
-fn five() -> i32 {
-    5
-}
-
-fn plus_one(y:i32) -> i32 {
-    y + 1
-}
-
-fn two() -> u32 {
-    2
-}
-
-fn minus_three(n:i32) -> i32 {
-    n - 3
+    // hello, world
 }
