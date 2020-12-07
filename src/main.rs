018b6dab8cb2f9b3989a674deb764ab7e594ab49
GreedoRen <greedo.it@gmail.com>
2020-12-07 22:32:16 +0300

    return value and scope

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
 fn main() {
-    let s = String::from("hello");
+    let s1 = gives_ownership();
 
-    takes_onwnership(s);
+    let s2 = String::from("hello");
 
-    let x = 5;
-
-    makes_copy(x);
-
-    //println!("{}, {}", s, x); // x -> s is no longer viable
+    let s3 = takes_and_gives_back(s2);
 }
 
-fn takes_onwnership(some_string: String) {
-    println!("{}", some_string);
-} 
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
 
-fn makes_copy(some_integer: i32) {
-    println!("{}", some_integer);
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
