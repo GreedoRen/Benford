23a7db89e9686484837883065f71a0b482f793cb
GreedoRen <greedo.it@gmail.com>
2020-12-14 23:46:57 +0300

    slice i32 array

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,5 @@
 fn main() {
-    let mut my_string = String::from("hello world");
+    let a = [1, 2, 3, 4, 5];
 
-    let word = first_word(&my_string[..]);
-
-    let string_literal = "hello world";
-
-    let word = first_word(&string_literal[..]);
-
-    let word = first_word(string_literal);
-
-    println!("the first word is: {}", word);
-}
-
-fn first_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
-
-    for(i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
-    }
-
-    &s[..]
+    let slice = &a[1..2];
 }
