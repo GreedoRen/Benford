3f4366e7f870253ec2fa5f645997421eb0f4a393
GreedoRen <greedo.it@gmail.com>
2020-12-14 23:21:42 +0300

    finded empty string

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 fn main() {
+    let mut s = String::from("hello world");
 
+    let word = first_word(&s);
+
+    println!("{}", word);
+
+    s.clear();
 }
 
 fn first_word(s: &String) -> usize {
     let bytes = s.as_bytes();
 
-    for(i, &item) in bytes.iter().enumerate() {
+    for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
             return i;
         }
@@ -13,4 +19,3 @@ fn first_word(s: &String) -> usize {
 
     s.len()
 }
-
