8ac06ba863a14fa6c5531b06a5c6a4023d9977f2
GreedoRen <greedo.it@gmail.com>
2020-12-14 23:25:45 +0300

    slice

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,8 @@
 fn main() {
-    let mut s = String::from("hello world");
+    let s = String::from("hello world");
 
-    let word = first_word(&s);
+    let hello = &s[0..5];
+    let world = &s[6..11]; 
 
-    println!("{}", word);
-
-    s.clear();
-}
-
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return i;
-        }
-    }
-
-    s.len()
+    println!("{}, {}", hello, world);
 }
