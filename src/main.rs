0fafd27d91d488a14c64cab86cc67b1e97906aca
GreedoRen <greedo.it@gmail.com>
2020-12-15 23:08:01 +0300

    struct update syntax

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-fn main() {}
+fn main() {
+    let user1 = User {
+        username: String::from("some_username"),
+        email: String::from("some_email@gmail.com"),
+        sign_in_count: 1,
+        active: true,
+    }
+
+    let user2 = User {
+        username: String::from("anoter_username"),
+        email: String::from("another_email@gmail.com"),
+        active: user1.active,
+        sign_in_count: user1.sign_in_count,
+    }
+}
 
 struct User {
     username: String,
@@ -9,8 +23,8 @@ struct User {
 
 fn build_user(email: String, username: String) -> User {
     User {
-        username: username,
-        email: email,
+        username,
+        email,
         sign_in_count: 1,
         active: true,
     }
