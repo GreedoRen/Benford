d65cff3787dd04cfc12edc810deae65f9719b4f5
GreedoRen <greedo.it@gmail.com>
2020-12-15 22:58:33 +0300

    mut user1

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
-   let user1 = User {
-       email: String::from("some_emain@gmail.com"),
+   let mut user1 = User {
+       email: String::from("some_email@gmail.com"),
        username: String::from("some_username"),
        active: true,
        sign_in_count: 1,
    };
+
+   user1.email = String::from("second_email@gmail.com");
+   
+   println!("{}", user1.email); 
 }
 
 struct User {
