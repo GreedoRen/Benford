498d0d27bf596dae641e5c642d405d8266aa67ed
GreedoRen <greedo.it@gmail.com>
2020-12-15 23:08:48 +0300

    struct update syntax spread

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,13 @@ fn main() {
         email: String::from("some_email@gmail.com"),
         sign_in_count: 1,
         active: true,
-    }
+    };
 
     let user2 = User {
         username: String::from("anoter_username"),
         email: String::from("another_email@gmail.com"),
-        active: user1.active,
-        sign_in_count: user1.sign_in_count,
-    }
+        ..user1
+    };
 }
 
 struct User {
