2126b0f14b99d1db0fd63b65dd9c1fe9db5eab55
GreedoRen <greedo.it@gmail.com>
2020-12-15 22:56:02 +0300

    user1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
-   
+   let user1 = User {
+       email: String::from("some_emain@gmail.com"),
+       username: String::from("some_username"),
+       active: true,
+       sign_in_count: 1,
+   };
 }
 
 struct User {
