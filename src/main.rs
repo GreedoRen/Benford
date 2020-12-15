1e1f0d302b02709bc631e9999aec715c797e990b
GreedoRen <greedo.it@gmail.com>
2020-12-15 23:02:29 +0300

    return strunct in fn

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-fn main() {
-   let mut user1 = User {
-       email: String::from("some_email@gmail.com"),
-       username: String::from("some_username"),
-       active: true,
-       sign_in_count: 1,
-   };
-
-   user1.email = String::from("second_email@gmail.com");
-   
-   println!("{}", user1.email); 
-}
+fn main() {}
 
 struct User {
     username: String,
     email: String,
     sign_in_count: u64,
     active: bool,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        username: username,
+        email: email,
+        sign_in_count: 1,
+        active: true,
+    }
 }
