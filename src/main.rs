802a78e1a20a8abc141530186db2d25809489d37
GreedoRen <greedo.it@gmail.com>
2020-12-16 22:58:19 +0300

    debug struct

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
-fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    )
-}
+#[derive(Debug)]
 
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rec1 is {:?}", rect1);
 }
