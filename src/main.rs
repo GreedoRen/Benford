38adf6fa5c0bb82f216252f9d07b2d3864704a75
GreedoRen <greedo.it@gmail.com>
2020-12-16 22:47:16 +0300

    use tuples

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 fn main() {
-    let width = 30;
-    let height = 50;
-
+    let rect1 = (30, 50);
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width, height)
+        area(rect1)
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimension: (u32, u32)) -> u32 {
+    dimension.0 * dimension.1
 }
