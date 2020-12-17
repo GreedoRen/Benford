df78b21523da87f096e968a89b3fa325c8bcf650
GreedoRen <greedo.it@gmail.com>
2020-12-17 22:43:59 +0300

    associated function

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,20 @@ fn main() {
         height: 15,
     };
 
+    let sq = Rectangle::square(3);
+
     println!("Can rect1 hold rec2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rec3? {}", rect1.can_hold(&rect3));
 }
 
 impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
     fn area(&self) -> u32{
         self.width * self.height
     }
