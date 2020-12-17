f8dffc33d83aac1e414cf64e20cb4dafd4b61d27
GreedoRen <greedo.it@gmail.com>
2020-12-17 22:34:41 +0300

    impl method

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,32 @@ struct Rectangle {
     height: u32,
 }
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-}
-
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
-    println!("rec1 is {:#?}", rect1.area());
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 88,
+        height: 15,
+    };
+
+    println!("Can rect1 hold rec2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rec3? {}", rect1.can_hold(&rect3));
+}
+
+impl Rectangle {
+    fn area(&self) -> u32{
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
