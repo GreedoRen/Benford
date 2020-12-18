4abea7bc61d7311178e520da6378471b9c00dbab
GreedoRen <greedo.it@gmail.com>
2020-12-18 23:05:35 +0300

    struct

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-fn main() {
-    let v4 = IpAddrKind::V4;
-    let v6 = IpAddrKind::V6;
+enum IpAddrKind {
+    V4,
+    V6
+}
 
-    route(IpAddrKind::V4);
-    route(IpAddrKind::V6);
+struct IpAddr {
+    kind: IpAddrKind,
+    adress: String,
 }
 
 fn route(ip_kind: IpAddrKind) {
 
 }
 
-enum IpAddrKind {
-    V4,
-    V6
+
+fn main() {
+    let v4 = IpAddrKind::V4;
+    let v6 = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+    
+    let home = IpAddr{
+        kind: IpAddrKind::V4,
+        adress: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        adress: String::from("::1"),
+    };
 }
