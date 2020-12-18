4bc0aa973b86908f5b20a8efe66752baa359edc3
GreedoRen <greedo.it@gmail.com>
2020-12-18 23:00:00 +0300

    fn

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
     let v4 = IpAddrKind::V4;
     let v6 = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+}
+
+fn route(ip_kind: IpAddrKind) {
+
 }
 
 enum IpAddrKind {
