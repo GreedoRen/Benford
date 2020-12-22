2dd3f1dcdac1c101581c7badd2f059ed37874948
GreedoRen <greedo.it@gmail.com>
2020-12-22 22:56:01 +0300

    del

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,1 @@
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
-}
-
-enum Option<T> {
-    Some(T),
-    None,
-}
-
-fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-
-    let loopback = IpAddr::V6(String::from("::1"));
-
-    let some_number = Some(5);
-    let some_string = Some("a string");
-}
+fn main() {}
