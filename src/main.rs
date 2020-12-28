708c2d9be33c3f8553f21eed7a3ce84c0aae8ef2
GreedoRen <greedo.it@gmail.com>
2020-12-28 21:55:34 +0300

    del

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,3 @@ fn main() {}
 
 
 
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        }
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        }
-    }
-}
